261e0cf897272e8b5d7643f2cd17abdfb64bed83
Viliam Kois <viliam.kois@sfb.sk>
2013-02-27 16:55:30 +0100

    copyright added

--- src/main/java/sk/wlio/sx2/readers/instrukcia/DeklaraciaPrikazReader.java
+++ src/main/java/sk/wlio/sx2/readers/instrukcia/DeklaraciaPrikazReader.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright viliam.kois@gmail.com Kois Viliam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package sk.wlio.sx2.readers.instrukcia;
 
 import sk.wlio.sx2.TextContext;
@@ -15,7 +30,7 @@ public class DeklaraciaPrikazReader implements TextReader<DeklaraciaPrikaz>  {
     public DeklaraciaPrikaz citaj(TextContext tC)  {
        DatovyTyp datovyTyp = Readers.datovyTyp().citaj( tC);
        String sDatovyTyp = datovyTyp.toString();
-       if (!RezervovaneSlovaEnum.DATOVY_TYP.je( sDatovyTyp))
+       if (!RezervovaneSlovaEnum.DATOVY_TYP.je(sDatovyTyp))
            throw SxException.create(SxExTyp.CAKAL_DEKLARACIU_PRIKAZU, tC);
 
        if ( !tC.jePrefixPrikaz())
